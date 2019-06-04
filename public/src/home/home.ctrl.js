4f39cac039ef08782803cbad4927690d2cb3ffda
Anthony Nixon <ajn0592@gmail.com>
2019-06-03 22:13:59 -0500

    show user's sets when they log in

--- public/src/home/home.ctrl.js
+++ public/src/home/home.ctrl.js
@@ -1,4 +1,12 @@
-angular.module('setsisaw').controller('HomeController', function($scope, $rootScope) {
+angular.module('setsisaw').controller('HomeController', function($scope, $rootScope, SetsService) {
     console.log("In the home controller. Hi");
-
+    $scope.loading = true;
+    $scope.sets = [];
+    SetsService.GetUserSets(function (response) {
+        if (response !== false) {
+            $scope.loading = false;
+            console.log(response);
+            $scope.sets = response;
+        }
+    })
 });
---
+++ public/src/services/sets.svc.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular
+    .module('setsisaw')
+    .factory('SetsService', Service);
+
+function Service($http) {
+    var service = {};
+
+    service.GetUserSets = GetUserSets;
+
+    return service;
+
+    function GetUserSets(callback) {
+        $http.get('https://api.setsisaw.com/sets')
+            .then(function (response) {
+                // login successful if there's a token in the respon
+                console.log("Response", response);
+                if (response.status === 200) {
+                    console.log(response.data);
+                    // execute callback with true to indicate successful login
+                    callback(response.data.sets);
+                } else {
+                    // execute callback with false to indicate failed login
+                    callback(false);
+                }
+            });
+    }
+
+    // function Logout() {
+    //     // remove user from local storage and clear http auth header
+    //     delete $localStorage.currentUser;
+    //     $http.defaults.headers.common.Authorization = '';
+    // }
+}
