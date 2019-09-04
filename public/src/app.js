fcda817a6758ffebc053f2eca89a69e95f63a956
Anthony Nixon <ajnixon@spscommerce.com>
2019-09-04 12:39:31 -0500

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -38,9 +38,9 @@ run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
 function run($rootScope, $location, $cookies, $http) {
     // keep user logged in after page refresh
     $rootScope.globals = $cookies.getObject('globals') || {};
-    if ($rootScope.globals.currentUser) {
-        $http.defaults.headers.common['Authorization'] = 'Bearer ' + $rootScope.globals.currentUser.authdata;
-    }
+//     if ($rootScope.globals.currentUser) {
+//         $http.defaults.headers.common['Authorization'] = 'Bearer ' + $rootScope.globals.currentUser.authdata;
+//     }
 
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
         // redirect to login page if not logged in and trying to access a restricted page
